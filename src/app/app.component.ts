c8ac643c56c909f520175022e1db8a951b56619b
Anna Michalik <anna.michalik@bluecrestinc.com>
2024-02-16 15:37:31 +0100

    mat-datepicker format works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import {Component} from '@angular/core';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatNativeDateModule} from '@angular/material/core';
+import {DateAdapter, MAT_DATE_FORMATS, MatNativeDateModule} from '@angular/material/core';
 import {NGX_MAT_DATE_FORMATS, NgxMatDatetimePickerModule} from "@angular-material-components/datetime-picker";
-import {CUSTOM_MAT_DATE_TIME_FORMATS} from "./formats";
+import {CUSTOM_MAT_DATE_FORMATS, CUSTOM_MAT_DATE_TIME_FORMATS} from "./formats";
+import {MomentDateAdapter} from "@angular/material-moment-adapter";
 
 @Component({
   selector: 'app-root',
@@ -14,7 +15,12 @@ import {CUSTOM_MAT_DATE_TIME_FORMATS} from "./formats";
   imports: [MatFormFieldModule, MatInputModule, MatDatepickerModule,
     MatNativeDateModule, NgxMatDatetimePickerModule],
   providers: [
-    {provide: NGX_MAT_DATE_FORMATS, useValue: CUSTOM_MAT_DATE_TIME_FORMATS}
+    {
+      provide: DateAdapter,
+      useClass: MomentDateAdapter
+    },
+    {provide: MAT_DATE_FORMATS, useValue: CUSTOM_MAT_DATE_FORMATS},
+    {provide: NGX_MAT_DATE_FORMATS, useValue: CUSTOM_MAT_DATE_TIME_FORMATS},
   ]
 })
 export class AppComponent {
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,17 @@
 import {bootstrapApplication} from '@angular/platform-browser';
-import {MAT_DATE_FORMATS, MatNativeDateModule} from '@angular/material/core';
+import {MAT_DATE_LOCALE, MatNativeDateModule} from '@angular/material/core';
 import {AppComponent} from "./app/app.component";
 import {provideAnimations} from "@angular/platform-browser/animations";
 import {provideHttpClient} from "@angular/common/http";
 import {importProvidersFrom} from "@angular/core";
-import {NGX_MAT_DATE_FORMATS, NgxMatNativeDateModule} from "@angular-material-components/datetime-picker";
-import {CUSTOM_MAT_DATE_FORMATS, CUSTOM_MAT_DATE_TIME_FORMATS} from "./app/formats";
+import {NgxMatDatetimePickerModule, NgxMatNativeDateModule} from "@angular-material-components/datetime-picker";
 
 
 bootstrapApplication(AppComponent, {
   providers: [
+    importProvidersFrom(MatNativeDateModule, NgxMatNativeDateModule, NgxMatDatetimePickerModule),
+    {provide: MAT_DATE_LOCALE, useValue: 'en-GB'},
     provideAnimations(),
     provideHttpClient(),
-    importProvidersFrom(MatNativeDateModule, NgxMatNativeDateModule),
-    {provide: NGX_MAT_DATE_FORMATS, useValue: CUSTOM_MAT_DATE_TIME_FORMATS},
-    {provide: MAT_DATE_FORMATS, useValue: CUSTOM_MAT_DATE_FORMATS}
   ]
 }).catch(err => console.error(err));
