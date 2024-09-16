8ba05f5edf28c531ad827e149703cba68fd2d996
Anna Michalik <anna.michalik@bluecrestinc.com>
2024-09-16 15:55:41 +0200

    only data for tree

--- src/app/component/tree5-levels-deep/tree5-levels-deep.component.ts
+++ src/app/component/tree5-levels-deep/tree5-levels-deep.component.ts
@@ -1,13 +1,55 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {MatTreeModule, MatTreeNestedDataSource} from "@angular/material/tree";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import {NestedTreeControl} from "@angular/cdk/tree";
+
+/**
+ * Food data with nested structure.
+ * Each node has a name and an optional list of children.
+ */
+interface FoodNode {
+  name: string;
+  children?: FoodNode[];
+}
+
+const TREE_DATA: FoodNode[] = [
+  {
+    name: 'Fruit',
+    children: [{name: 'Apple'}, {name: 'Banana'}, {name: 'Fruit loops'}],
+  },
+  {
+    name: 'Vegetables',
+    children: [
+      {
+        name: 'Green',
+        children: [{name: 'Broccoli'}, {name: 'Brussels sprouts'}],
+      },
+      {
+        name: 'Orange',
+        children: [{name: 'Pumpkins'}, {name: 'Carrots'}],
+      },
+    ],
+  },
+];
 
 @Component({
   selector: 'app-tree5-levels-deep',
   standalone: true,
-  imports: [CommonModule],
+  imports: [MatTreeModule, MatButtonModule, MatIconModule, MatTreeModule, MatIconModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './tree5-levels-deep.component.html',
   styleUrls: ['./tree5-levels-deep.component.scss']
 })
 export class Tree5LevelsDeepComponent {
+  treeControl = new NestedTreeControl<FoodNode>((node: FoodNode) => node.children);
+  dataSource = new MatTreeNestedDataSource<FoodNode>();
 
+  constructor() {
+    this.dataSource.data = TREE_DATA;
+  }
+
+  hasChild = (_: number, node: FoodNode) => !!node.children && node.children.length > 0;
 }
+
